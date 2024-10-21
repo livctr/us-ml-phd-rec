e40e7ff8862a8aa8515735a5b196c3e60042e374
liv20 <vhli2020@gmail.com>
2024-10-21 19:20:48 -0400

    add streamlit frontend stuff

---
+++ streamlit.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+from core.recommender import EmbeddingProcessor, Recommender
+
+st.title("U.S. ML PhD Faculty Advisor Recommender")
+
+# Define CSS for light and dark mode toggle
+def load_css(css_file):
+    with open(css_file, "r") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css("frontend/styles.css")
+
+
+# Set up
+embedding_processor = EmbeddingProcessor()
+recommender = Recommender(embedding_processor)
+
+# Query input field
+query = st.text_input("Name an ML research area you are interested in (e.g. low-rank adaptation)")
+
+# Search and display professors
+if query:
+    top_k_indices = recommender.get_top_k(query, top_k=10)
+    professors_data = recommender.get_recommended_data(top_k_indices)
+
+    if professors_data:
+        for professor_data in professors_data:
+            st.subheader(professor_data["name"])
+            st.write(f"{professor_data['title']} of {professor_data['department']}, {professor_data['university']}")
+
+            # List of papers
+            st.write("Most Relevant Papers:")
+            for paper in professor_data["papers"]:
+                st.markdown(f"- [{paper[1]}](https://arxiv.org/abs/{paper[0]})")
+    else:
+        st.write("No results found for your query.")
